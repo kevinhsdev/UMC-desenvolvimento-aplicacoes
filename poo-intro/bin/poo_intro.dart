66b95a6229511f4c4d885d9350ec15c6e43a9fd1
aluno cmc <alunocmc@umc.br>
2026-08-25 10:06:18 -0300

    add Movie class and print info in the main

--- poo-intro/bin/poo_intro.dart
+++ poo-intro/bin/poo_intro.dart
@@ -1,6 +1,49 @@
-import 'dart:io'
-
 void main(){
-  
-};
+  print("");
 
+  
+  Movie spiderMan = Movie(
+    title: "Spider-Man - Brand New Day",
+    genre: "Super-Hero", 
+    classification: 12,
+    duration: 145
+  );
+
+  spiderMan.infoMovie();
+
+  print("");
+
+  Movie avengers = Movie(
+    title: "Avengers - Ultimato", 
+    genre: "Action",
+    classification: 14,
+    duration: 181
+  );
+
+  avengers.infoMovie();
+
+  print("");
+}
+//Class creation
+class Movie {
+
+  //Set attributes
+  String title;
+  String genre;
+  int classification;
+  int duration;
+
+  //Constructor
+  Movie({
+    required this.title, 
+    required this.genre, 
+    required this.classification, 
+    required this.duration
+  });
+
+  //Create methods
+  void infoMovie() {
+    print("Title: $title, Genre: $genre, Classification: $classification years old, duration: $duration minutes.");
+  }
+
+}
